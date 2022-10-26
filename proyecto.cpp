f44bf2a6354ad73d43ee1c98f1d1e13a46a988dc
franco <fraan>
2022-10-26 20:12:33 -0300

    Proyecto v2

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
+struct dias
+{
+    int dia;
+    int mes;
+    int horas;
+    int minutos;
+};
+
+struct clima
+{
+    float temp;
+    float humedad;
+    struct dias time;
+};
+
+struct ciudad
+{
+    struct ciudad *next;
+    struct clima c;
+    int ciudad_id;
+    char nombre_ciudad[50];
+};
+
 int main(int argc, char *argv[]){
+    struct ciudad p;
 
 
 
